cf63b24b76b506363c1e2d399343b21d0e760084
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2026-03-22 22:05:15 +0700

    Add files via upload

--- include/db_config.php
+++ include/db_config.php
@@ -24,6 +24,7 @@ function getDBConnection() {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci",
             ];
             $conn = new PDO($dsn, DB_USER, DB_PASS, $options);
         } catch (PDOException $e) {
